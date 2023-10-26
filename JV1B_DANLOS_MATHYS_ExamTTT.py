f7f07d7c5f87cf955967b3fd36ae31abe0795651
Matte-sama <148343105+Matte-sama@users.noreply.github.com>
2023-10-26 17:10:24 +0200

    Add files via upload

---
+++ JV1B_DANLOS_MATHYS_ExamTTT.py
@@ -0,0 +1,27 @@
+#exercice 1
+
+# 1 | 2 | 3
+#----------
+# 3 | 4 | 5
+#----------
+# 6 | 7 | 8
+
+
+n = 0
+tableau = [[n]]*3
+for i in range (3):
+    n = n+1
+    print (tableau)
+
+#exercice 2
+
+#exercice 3
+
+#exercice 4
+
+#exercice 5
+
+# exercice 6
+
+
+
